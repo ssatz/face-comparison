9a7d7ae6b0691b6d75c354fb166da112d6de8c27
soft_developer_edg <110408089+EduardoGuevara124@users.noreply.github.com>
2023-09-22 14:48:04 +0700

    Update app.py (#8)
    Added 1 method to upload original image.

--- upload/app.py
+++ upload/app.py
@@ -18,9 +18,22 @@ def upload():
 
     source_string = base64_images['SourceImage[Bytes]']
     target_string = base64_images['TargetImage[Bytes]']
+
     similarity_threshold = base64_images['SimilarityThreshold']
 
     face_similarity = FaceSimilarity(source_string, target_string, similarity_threshold)
 
     return json.dumps(face_similarity.response_data)
 
+
+@app.route("/api/upload/images", methods=["POST"])
+def upload_images():
+    """Handle the upload of a file."""
+    source_image = request.files['source_image']
+    target_image = request.files['target_image']
+
+    similarity_threshold = request.form['SimilarityThreshold']
+
+    face_similarity = FaceSimilarity(source_image, target_image, similarity_threshold, is_base64=False)
+
+    return json.dumps(face_similarity.response_data)
